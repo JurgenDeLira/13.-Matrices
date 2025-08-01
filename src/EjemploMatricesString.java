b2347e0f9c19ba572deb60e65cbbe4daf2203148
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-07-31 23:04:46 -0700

    3

--- src/EjemploMatricesString.java
+++ src/EjemploMatricesString.java
@@ -4,12 +4,26 @@ public class EjemploMatricesString {
         nombres[0][0] = "Pepe";
         nombres[0][1] = "Pepa";
         nombres[1][0] = "Josefa";
-        nombres[1][1] = "Paco";
-        nombres[2][0] = "Lucas Podolski";
+        nombres[1][1] = "Lucas Podolski";
+        nombres[2][0] = "Pipo";
         nombres[2][1] = "Pabliski";
 
+        System.out.println("Iterando con for: ");
         for (int i = 0; i < nombres.length; i++){
-            for (int j = 0; j <)
+            for (int j = 0; j <nombres[i].length; j++) {
+                System.out.print(nombres[i][j] + "\t");
+            }
+            System.out.println();
         }
+
+        System.out.println("Iterando con foreach: ");
+
+        for(String[] fila: nombres){
+            for(String nombre: fila){
+                System.out.print(nombre + "\t");
+            }
+            System.out.println();
+        }
+
     }
 }
